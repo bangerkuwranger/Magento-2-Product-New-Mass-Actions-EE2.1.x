6fd09f3719a0eaa9a9d651c1d697b463340dd6cc
Chad A. Carino <ccarino@sundialbrands.com>
2017-03-29 15:00:05 -0400

    Revert "sure…."
    This reverts commit 58d093db96ee7d58cf6f48c65726b3632442ab1f.

--- Observer/SetNewStatus.php
+++ Observer/SetNewStatus.php
@@ -2,7 +2,7 @@
 
 namespace Bangerkuwranger\Productnewmassactions\Observer;
 use Magento\Framework\Event\ObserverInterface;
-use Bangerkuwranger\Productnewmassactions\Logger\Logger;
+use Psr\Log\LoggerInterface;
 
 class SetNewStatus implements ObserverInterface {
 
@@ -12,19 +12,19 @@ class SetNewStatus implements ObserverInterface {
      */
 	protected $_timezone;
 	/**
-     * @var Logger
+     * @var LoggerInterface
      */
-    protected $_logger;
+    protected $_loggerInterface;
     
     public function __construct(
 		\Magento\Framework\Stdlib\DateTime\TimezoneInterface $_timezone,
 		\Magento\Framework\App\RequestInterface $_request,
-		Logger $_logger
+		LoggerInterface $_loggerInterface
 	) {
     
 		$this->_timezone = $_timezone;
 		$this->_request = $_request;
-		$this->_logger = $_logger;
+		$this->_loggerInterface = $_loggerInterface;
 
     }
     
@@ -34,34 +34,19 @@ class SetNewStatus implements ObserverInterface {
      */
     public function execute( \Magento\Framework\Event\Observer $_observer ) {
     
-//     	$request =  $_observer->getRequest();
-		$postData =  $this->_request->getPostValue();
-// 		$this->_logger->info( 'observer req: ' /* . print_r( $request, true )*/ );
-		
+        $request = $_observer->getEvent()->getRequest();
+        $this->_loggerInterface->debug(print_r($request,true));
+		$isNew = $this->_request->getPost('product[is_new]');
         $product = $_observer->getEvent()->getProduct();
-//         $this->_logger->info( 'observer req: '  . print_r( $product, true ) );
-//         $newChange = $product->dataHasChangedFor( 'is_new' );
-//         $this->_logger->info( 'observer req: '  . print_r( $postData, true ) );
-//         return;
-// 		return $this;
-		$isNew = $postData['product']['is_new'];
-// 		print_r( $isNew );
-// 		exit();
-// 		$this->_logger->info( "observer is_new changed: " . $isNew . "\n" );
-		$product->setData( 'news_from_date', '' );
-		$product->setData( 'news_to_date', '' );
-		$product->setData( 'is_new', 0 );
-		$this->_request->setPostValue( 'news_from_date', '' );
-		$this->_request->setPostValue( 'news_to_date', '' );
-		if( 1 === $isNew ) {
-	
-			$tz = new \DateTimeZone( $this->_timezone->getConfigTimezone() );
-			$now = new \DateTime( null, $tz );
-			$product->setData( 'news_from_date', $now->format( "Y-m-d H:i:s" ) );
-			$this->_request->setPostValue( 'news_from_date', $now->format( "Y-m-d H:i:s" ) );
-	
-		}
-// 		$product->save();
+        $product->setData( 'news_from_date', '' );
+        $product->setData( 'news_to_date', '' );
+        if( !$isNew ) {
+        
+            $tz = new \DateTimeZone( $this->_timezone->getConfigTimezone() );
+            $now = new \DateTime( null, $tz );
+            $product->setData( 'news_from_date', $now->format( "Y-m-d h:i:s" ) );
+        
+        }
     
     }
 
